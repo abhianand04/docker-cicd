9ea379d2c6ab2d8af1281298f97b26a36bd1888f
Abhishek Anand <abhishekanand@aishware.tech>
2026-02-28 10:22:54 +0530

    updated in index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker 🚀");
+  res.send("Hello from Docker and this push i am doing through cicd 🚀");
 });
 
 app.listen(9000, () => {
